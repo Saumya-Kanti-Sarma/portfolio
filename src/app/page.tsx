6a7653e8abf09692615e42162481b6bb25ebb188
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-06-11 11:06:26 +0530

    little changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
-import Navbar from "@/components/navbar/Navbar.component";
-import Image from "next/image";
-
 export default function Home() {
   return (
     <>
-      <Navbar />
       <h1>hello</h1>
     </>
   );
