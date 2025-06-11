ea26b0ee401f8263ebeb81c07394e90785b54593
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-06-11 10:14:57 +0530

    imporved global css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Navbar from "@/components/navbar/Navbar.component";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <h1>hello</h1>
     </>
   );
